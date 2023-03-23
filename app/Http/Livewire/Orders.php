579032083c20e3a71fc0930dc29ad0dd73d4c9da
Markov Andrey <andremarkov@icloud.com>
2023-03-23 18:01:05 +0300

    Change of order status with editing in the database, additional visual wrapper depending on the status of the order, a minor layout error appears when changing the status due to Livewire rendering

--- app/Http/Livewire/Orders.php
+++ app/Http/Livewire/Orders.php
@@ -9,13 +9,20 @@ use Livewire\Component;
 
 class Orders extends Component
 {
-    public $currentStatus;
+    /**
+     * Selected status parameter
+     * @var array
+     */
+    public $selectedOption = [];
 
+    /**
+     * @param $orderProducts - Order Collection
+     */
     public function sendStatus($orderProducts)
     {
         foreach ($orderProducts as $item) {
             $order = Order::findOrFail($item['id']);
-            $order->status = $this->currentStatus;
+            $order->status = $this->selectedOption[$order->order];
             $order->save();
         }
     }
@@ -26,14 +33,34 @@ class Orders extends Component
             ->leftJoin('users', 'orders.user_id', '=', 'users.id')
             ->leftJoin('products', 'orders.product_id', '=', 'products.id')
             ->leftJoin('statuses', 'orders.status', '=', 'statuses.id')
-            ->select('orders.*', 'users.name as user_name', 'users.email as user_email', 'products.title as product_title', 'statuses.status_name as status_name')
+            ->select(
+                'orders.*',
+                'users.name as user_name',
+                'users.email as user_email',
+                'products.title as product_title',
+                'products.price as product_price',
+                'statuses.status_name as status_name')
             ->orderBy('orders.created_at', 'DESC')
             ->paginate(10);
+
+        //All status table
         $statuses = Status::query()->get();
+
+        //Total Sum
+        $total = null;
+        foreach ($orders as $item){
+            $total += $item->product_price * $item->quantity;
+        }
+
+        //Update ticket status
+        foreach($orders as $order){
+            $this->selectedOption[$order->order] = $order->status;
+        }
 
         return view('livewire.orders', [
             'orders' => $orders,
             'statuses' => $statuses,
+            'total' => $total,
         ]);
     }
 }
--- resources/views/livewire/orders.blade.php
+++ resources/views/livewire/orders.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{}">
+<div x-data="{open: false}">
     <div class="container mx-auto px-6 py-8">
         <h3 class="text-gray-700 text-3xl font-medium">Orders</h3>
 
@@ -6,7 +6,45 @@
             @foreach($orders->groupBy('order') as $orderNum => $orderProducts)
 
             <h2 id="accordion-arrow-icon-heading-{{ $loop->iteration }}">
-                <button type="button" class="flex gap-x-4 items-center justify-between w-full p-5 font-medium text-left text-gray-900 bg-yellow-100 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-white dark:bg-gray-800 active:bg-yellow-300 hover:bg-yellow-200 dark:hover:bg-gray-800" data-accordion-target="#accordion-arrow-icon-body-{{ $loop->iteration}}" aria-expanded="true" aria-controls="accordion-arrow-icon-body-{{ $loop->iteration}}">
+                <button type="button" class="flex gap-x-4 items-center justify-between w-full p-5 font-medium text-left text-gray-900 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-white dark:bg-gray-800 focus:ring-gray-200
+                @switch($orderProducts->first()->status)
+                    @case(1)
+                        bg-red-400
+                        hover:bg-red-500
+                        active:bg-red-600
+                    @break
+                    @case(2)
+                        bg-orange-300
+                        hover:bg-orange-400
+                        active:bg-orange-500
+                    @break
+                    @case(3)
+                        bg-yellow-100
+                        hover:bg-yellow-200
+                        active:bg-yellow-300
+                    @break
+                    @case(4)
+                        bg-blue-200
+                        hover:bg-blue-300
+                        active:bg-blue-400
+                    @break
+                    @case(5)
+                        bg-green-400
+                        hover:bg-green-500
+                        active:bg-green-600
+                    @break
+                    @case(6)
+                        bg-gray-300
+                        hover:bg-gray-400
+                        active:bg-gray-500
+                    @break
+                    @case(7)
+                        bg-gray-300
+                        hover:bg-gray-400
+                        active:bg-gray-500
+                    @break
+                @endswitch
+                    " data-accordion-target="#accordion-arrow-icon-body-{{ $loop->iteration}}" aria-expanded="true" aria-controls="accordion-arrow-icon-body-{{ $loop->iteration}}" @click="open = !open">
                     <div class="flex gap-x-4 flex-col text-center">
                         <span>№</span>
                         <span>{{ $orderNum }}</span>
@@ -35,7 +73,7 @@
                 </button>
             </h2>
 
-            <div id="accordion-arrow-icon-body-{{ $loop->iteration}}" aria-labelledby="accordion-arrow-icon-heading-1">
+            <div id="accordion-arrow-icon-body-{{ $loop->iteration }}" aria-labelledby="accordion-arrow-icon-heading-{{ $loop->iteration }}">
                 <table class="min-w-full">
                     <thead>
                     <tr>
@@ -44,6 +82,12 @@
                         </th>
                         <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                             Quantity
+                        </th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Price
+                        </th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                            Subtotal
                         </th>
                     </tr>
                     </thead>
@@ -56,14 +100,20 @@
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                 <div class="text-sm leading-5 text-gray-900">{{ $order->quantity }}</div>
                             </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">{{ $order->product_price }}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">{{ $order->product_price * $order->quantity }}</div>
+                            </td>
                         </tr>
                     </tbody>
 
                 @endforeach
-                    <tfoot>
+                    <tfoot class="bg-white">
                         <tr>
-                            <td>
-                                <select wire:model="currentStatus" wire:change="sendStatus({{ $orderProducts }})" id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <select wire:model="selectedOption.{{ $orderNum }}" wire:change="sendStatus({{ $orderProducts }})" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     @foreach($statuses as $status)
                                         <option
                                             @if($status->id == $orderProducts->first()->status)
@@ -74,6 +124,16 @@
                                         </option>
                                     @endforeach
                                 </select>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+
+                            </td>
+                            <td class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                TOTAL:
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                {{ $total }}
                             </td>
                         </tr>
                     </tfoot>
