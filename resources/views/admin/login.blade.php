4062968e3963a044762e822043108aa4fa7da67e
Markov Andrey <andremarkov@icloud.com>
2023-04-01 18:31:05 +0300

    Fix admin login form

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,18 +5,19 @@
 @section('content')
     <div class="h-screen bg-white flex flex-col space-y-10 justify-center items-center">
         <div class="bg-white w-96 shadow-xl rounded p-5">
-            <h1 class="text-3xl font-medium">Login</h1>
+            <h1 class="text-3xl font-medium">Admin Login</h1>
+            <h2 class="text-xl font-medium italic">Welcome, boss!</h2>
 
             <form action="{{ route("admin.login_process") }}" method="POST" class="space-y-5 mt-5">
                 @csrf
 
-                <input name="email" type="text" class="w-full h-12 border border-red-500 rounded px-3" placeholder="Email" />
+                <input name="email" type="text" class="w-full h-12 border @error('email') border-red-500 @enderror rounded px-3" placeholder="Email" />
 
                 @error('email')
                     @include('partials.error', ['message' => $message])
                 @enderror
 
-                <input name="password" type="password" class="w-full h-12 border border-gray-800 rounded px-3" placeholder="Password" />
+                <input name="password" type="password" class="w-full h-12 border @error('password') border-red-500 @enderror border-gray-800 rounded px-3" placeholder="Password" />
 
                 @error('password')
                     @include('partials.error', ['message' => $message])
